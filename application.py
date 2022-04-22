2c274a2407785912798c35197060a446b2546a76
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-04-22 12:39:08 +0000

    clean codebase

--- application.py
+++ application.py
@@ -28,7 +28,6 @@ async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
     user = authenticate_user(db, form_data.username, form_data.password)
-    print(user)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
